e2a34d94f89556e05d08fbd585cc135a914862ed
Oliver Charles <ollie@ocharles.org.uk>
2013-07-24 14:39:54 +0000

    Update to work with Diff 0.2 and higher

--- src/Data/Algorithm/Diff3.hs
+++ src/Data/Algorithm/Diff3.hs
@@ -1,8 +1,9 @@
 {-| An implementation of a 3-way merge algorithm. -}
 module Data.Algorithm.Diff3 (Hunk(..), diff3, merge) where
 
-import Data.Algorithm.Diff
 import Data.Monoid (Monoid, mempty, mappend)
+
+import qualified Data.Algorithm.Diff as Diff
 
 --------------------------------------------------------------------------------
 -- | A hunk is a collection of changes that occur in a document. A hunk can be
@@ -15,7 +16,7 @@ data Hunk a = ChangedInA [a] | ChangedInB [a] | Both [a] | Conflict [a] [a] [a]
 --------------------------------------------------------------------------------
 -- | Perform a 3-way diff against 2 documents and the original document.
 diff3 :: Eq a => [a] -> [a] -> [a] -> [Hunk a]
-diff3 a o b = step (getDiff o a) (getDiff o b)
+diff3 a o b = step (Diff.getDiff o a) (Diff.getDiff o b)
   where
     step [] [] = []
     step [] ob = toHunk [] ob
@@ -38,48 +39,53 @@ merge hunks = maybe (Left hunks) Right $ go hunks
 
 
 --------------------------------------------------------------------------------
-toHunk :: [(DI, a)] -> [(DI, a)] -> [Hunk a]
+toHunk :: [Diff.Diff a] -> [Diff.Diff a] -> [Hunk a]
 toHunk [] [] = mempty
-toHunk a  [] = return $ ChangedInA $ map snd a
-toHunk [] b  = return $ ChangedInB $ map snd b
+toHunk a  [] = return $ ChangedInA $ map get a
+toHunk [] b  = return $ ChangedInB $ map get b
 toHunk a  b
-  | all isB a && all isB b = return $ Both $ map snd $ filter isA a
-  | all isB a = return $ ChangedInB $ map snd $ filter isA b
-  | all isB b = return $ ChangedInA $ map snd $ filter isA a
-  | otherwise = return $ Conflict (map snd $ filter isA a)
-                                  (map snd $ filter isO a)
-                                  (map snd $ filter isA b)
-
+  | all isB a && all isB b = return $ Both $ map get $ filter isA a
+  | all isB a = return $ ChangedInB $ map get $ filter isA b
+  | all isB b = return $ ChangedInA $ map get $ filter isA a
+  | otherwise = return $ Conflict (map get $ filter isA a)
+                                  (map get $ filter isO a)
+                                  (map get $ filter isA b)
 
 --------------------------------------------------------------------------------
-isA :: (DI, t) -> Bool
-isA (F,_) = False
-isA (_,_) = True
+get :: Diff.Diff t -> t
+get (Diff.First x) = x
+get (Diff.Second x) = x
+get (Diff.Both x _) = x
+
+--------------------------------------------------------------------------------
+isA :: Diff.Diff t -> Bool
+isA (Diff.First _) = False
+isA _ = True
 {-# INLINE isA #-}
 
 --------------------------------------------------------------------------------
-isO :: (DI, t) -> Bool
-isO (S,_) = False
-isO (_,_) = True
+isO :: Diff.Diff t -> Bool
+isO (Diff.Second _) = False
+isO _ = True
 {-# INLINE isO #-}
 
 
 --------------------------------------------------------------------------------
-isB :: (DI, t) -> Bool
-isB (B,_) = True
-isB (_,_) = False
+isB :: Diff.Diff t -> Bool
+isB (Diff.Both _ _) = True
+isB _ = False
 {-# INLINE isB #-}
 
 --------------------------------------------------------------------------------
-shortestMatch :: [(DI,a)] -> [(DI,a)] -> ([Hunk a], [(DI, a)], [(DI, a)])
+shortestMatch :: [Diff.Diff a] -> [Diff.Diff a] -> ([Hunk a], [Diff.Diff a], [Diff.Diff a])
 shortestMatch oa ob = go oa ob [] []
   where
-    go (x@(B,_):xs) (y@(B,_):ys) accX accY = go xs ys (accX ++ [x]) (accY ++ [y])
+    go (x@(Diff.Both _ _):xs) (y@(Diff.Both _ _):ys) accX accY = go xs ys (accX ++ [x]) (accY ++ [y])
     go xs ys accX accY = (toHunk accX accY, xs, ys)
 
 
 --------------------------------------------------------------------------------
-shortestConflict :: [(DI,a)] -> [(DI,a)] -> ([Hunk a], [(DI, a)], [(DI, a)])
+shortestConflict :: [Diff.Diff a] -> [Diff.Diff a] -> ([Hunk a], [Diff.Diff a], [Diff.Diff a])
 shortestConflict l r =
     let (hunk, rA, rB) = go l r
     in (uncurry toHunk hunk, rA, rB)
@@ -87,8 +93,8 @@ shortestConflict l r =
     go [] b = (([], b), [], [])
     go a [] = ((a, []), [], [])
     go a b =
-      let (as, ta) = break isBoth a
-          (bs, tb) = break isBoth b
+      let (as, ta) = break isB a
+          (bs, tb) = break isB b
           am = sum $ map motion as
           bm = sum $ map motion bs
           (as', ta') = incurMotion bm ta
@@ -97,19 +103,16 @@ shortestConflict l r =
          then ((as, bs), ta, tb)
          else ((as ++ as', bs ++ bs'), [], []) <> go ta' tb'
 
-    isBoth (B,_) = True
-    isBoth (_,_) = False
-
-    motion (S,_) = 0
+    motion (Diff.Second _) = 0
     motion _ = 1
 
 
 --------------------------------------------------------------------------------
-incurMotion :: Int -> [(DI, t)] -> ([(DI,t)], [(DI,t)])
+incurMotion :: Int -> [Diff.Diff t] -> ([Diff.Diff t], [Diff.Diff t])
 incurMotion _ [] = ([], [])
 incurMotion 0 as  = ([], as)
-incurMotion n (a@(B,_):as) = ([a], []) <> incurMotion (pred n) as
-incurMotion n (a@(S,_):as) = ([a], []) <> incurMotion (pred n) as
+incurMotion n (a@(Diff.Both _ _):as) = ([a], []) <> incurMotion (pred n) as
+incurMotion n (a@(Diff.Second _):as) = ([a], []) <> incurMotion (pred n) as
 incurMotion n (a:as) = ([a], []) <> incurMotion n as
 
 
